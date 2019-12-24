0b20bf2b7091653d240915334136360b45c8b931
Casey Key <caseykey@rams.colostate.edu>
2019-12-24 16:21:01 -0700

    only rendering the nav bar

--- src/App.js
+++ src/App.js
@@ -1,27 +1,15 @@
 import React, { Component } from 'react';
 import './App.css';
-import Sidebar from './components/sidebar'
-import Introduction from './components/introduction'
-import About from './components/about'
-import Projects from './components/projects'
-import Blog from './components/blog'
-import Timeline from './components/timeline'
+import Navigation from './components/Navigation'
 
 class App extends Component {
   render() {
     return (
       <div id="colorlib-page">
-        <div id="container-wrap">
-		<Sidebar></Sidebar>
-		<div id="colorlib-main">
-			<Introduction></Introduction>
-			<About></About>
-			<Projects></Projects>
-			<Blog></Blog>
-			<Timeline></Timeline>
-          	</div>
-      	</div>
-      </div>
+				<div id="container-wrap">
+					<Navigation></Navigation>
+				</div>
+			</div>
     );
   }
 }
