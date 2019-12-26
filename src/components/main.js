503e9468110fa925c2da77d2bb61632e9f7153a6
Casey Key <caseykey@rams.colostate.edu>
2019-12-25 19:51:50 -0700

    removed carousel

--- src/components/main.js
+++ src/components/main.js
@@ -95,32 +95,6 @@ import $ from 'jquery'
 	    });
 		});
 	};
-
-
-	var carousel = function() {
-		$('.home-slider').owlCarousel({
-	    loop:true,
-	    autoplay: true,
-	    margin:0,
-	    animateOut: 'fadeOut',
-	    animateIn: 'fadeIn',
-	    nav:false,
-	    autoplayHoverPause: false,
-	    items: 1,
-	    navText : ["<span class='ion-md-arrow-back'></span>","<span class='ion-chevron-right'></span>"],
-	    responsive:{
-	      0:{
-	        items:1
-	      },
-	      600:{
-	        items:1
-	      },
-	      1000:{
-	        items:1
-	      }
-	    }
-		});
-	};
 	
 
 	// scroll
@@ -298,7 +272,6 @@ export function initPage() {
 	//$.Scrollax();
 	burgerMenu();
 	onePageClick();
-	carousel();
 	scrollWindow();
 	counter();
 	contentWayPoint();
