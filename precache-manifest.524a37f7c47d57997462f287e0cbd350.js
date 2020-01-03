4b9b8798b994501e5bad93af5f1f125523395e44
Casey Key <caseykey@rams.colostate.edu>
2020-01-03 13:35:33 -0700

    Updates

---
+++ precache-manifest.524a37f7c47d57997462f287e0cbd350.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "65ba4655b43283a8465cc5f618ce1f46",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2c8cc70d31b81cf3075d",
+    "url": "/static/css/2.09ae8418.chunk.css"
+  },
+  {
+    "revision": "4b41564890a5ab94ae5b",
+    "url": "/static/css/main.6a934dfc.chunk.css"
+  },
+  {
+    "revision": "2c8cc70d31b81cf3075d",
+    "url": "/static/js/2.e03ab8e6.chunk.js"
+  },
+  {
+    "revision": "58601cf49df6a8ddd0ce8df224b1024a",
+    "url": "/static/js/2.e03ab8e6.chunk.js.LICENSE"
+  },
+  {
+    "revision": "4b41564890a5ab94ae5b",
+    "url": "/static/js/main.987b2274.chunk.js"
+  },
+  {
+    "revision": "2b2afb1a89412ee81749",
+    "url": "/static/js/runtime-main.d6f94871.js"
+  },
+  {
+    "revision": "7e55fc6d56fff2b0cf5ceadb34b01de6",
+    "url": "/static/media/about.7e55fc6d.png"
+  }
+]);
