c176874432b72232b7840424af44b5d217bd64b6
Casey Key <caseykey@rams.colostate.edu>
2020-01-06 23:00:16 -0700

    Applied new project format to all projects, need to finish the component

--- src/components/Projects/Project.jsx
+++ src/components/Projects/Project.jsx
@@ -1,17 +1,33 @@
-import React, { Component } from 'react'
-import { Card, CardText, CardSubtitle } from 'reactstrap'
+import React, { Fragment, useState } from 'react'
+import { Modal, ModalHeader, ModalFooter, ModalBody, Button } from 'reactstrap'
 import { StyledCard, Body, Header, Title, Subtitle, Text } from './style.js'
 
 // Using a functional component because we don't use state, constructor, or lifecycle hooks
-export default function Project({children, title, subtitle, icons}) {
+export default function Project({children, title, subtitle, icons, modalTitle, modalSubtitle, link}) {
+	const [modal, setModal] = useState(false);
+
+	const toggleModal = () => setModal(!modal);
 
   return (
-    <StyledCard className="shadow">
-      <Body className="p-0">
-        <Header className="text-center">{title}</Header>
-        <Subtitle>{subtitle}</Subtitle>
-        <Text>{children}</Text>
-      </Body>
-    </StyledCard>
+    <Fragment>
+      <div className="shadow project img  d-flex justify-content-center align-items-center" style={{backgroundImage: 'url(images/demo-02.png)'}}>
+        <div className="overlay" onClick={toggleModal}/>
+        <div className="text text-center p-4">
+          <h3>{title}</h3>
+          <span>{subtitle}</span>
+        </div>
+      </div>
+      <Modal isOpen={modal} toggle={toggleModal}>
+        <ModalHeader toggle={toggleModal}>
+          {modalTitle}
+        </ModalHeader>
+        <ModalBody>
+          {children} 
+        </ModalBody>
+        <ModalFooter>
+          <Button color="primary" href={link} target={"_blank"}>View project</Button>
+        </ModalFooter>
+      </Modal>
+    </Fragment>
   );
 }
