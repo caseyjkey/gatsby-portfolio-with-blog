2f94e843000de43ca52307d4d9d49a36766d379d
Casey Key <caseykey@rams.colostate.edu>
2020-01-14 13:34:25 -0700

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.4b6d3d760a9e62cc3cb13c76517e3372.js"
+  "/precache-manifest.4f69952c1dbe97ba7419e50c1498f111.js"
 );
 
 self.addEventListener('message', (event) => {
--- styles-43bde490674cd969af14.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[3],{lIpO:function(n,o,p){},sV4Q:function(n,o,p){}}]);
-//# sourceMappingURL=styles-43bde490674cd969af14.js.map
