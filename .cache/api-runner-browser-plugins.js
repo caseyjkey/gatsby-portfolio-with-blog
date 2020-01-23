fca7b9b7617d153fe7338752521d566bd3dcff97
Casey Key <caseykey@rams.colostate.edu>
2020-01-23 00:16:25 -0700

    changed labels and allowed adding images to gallery

---
+++ .cache/api-runner-browser-plugins.js
@@ -0,0 +1,10 @@
+module.exports = [{
+      plugin: require('../node_modules/gatsby-plugin-netlify-cms/gatsby-browser.js'),
+      options: {"plugins":[],"modulePath":"/home/kc/Development/portfolio/colorlib-spa/portfolio-app/src/cms/cms.js"},
+    },{
+      plugin: require('../node_modules/gatsby-plugin-netlify-cms/gatsby-browser.js'),
+      options: {"plugins":[]},
+    },{
+      plugin: require('../gatsby-browser.js'),
+      options: {"plugins":[]},
+    }]
