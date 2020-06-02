f46129aa84c66470d756c93e0565250e5c276dcd
Casey Key <casey.key@protonmail.com>
2020-06-01 23:56:38 -0600

    updated Icon attributes to use new method, tried updating react-icons

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -4,6 +4,7 @@ import { Container, Row, Col } from 'reactstrap'
 import { Heading } from './style'
 import { SkillsSection, SubsectionTitle } from './Skills/style'
 import { Skill } from './Skills/Skill'
+import { RiFlutterLine } from 'react-icons/ri'
 
 export default function Skills() {
 
@@ -12,40 +13,51 @@ export default function Skills() {
     // [return] Component
     function loadIcon(Icon) {
         let iconGroup = Icon.slice(0, 2);
+        console.log(iconGroup, Icon);
         if (iconGroup === 'Gi') {
-        return lazy(() => 
-            import('react-icons/gi').then(module => 
-            ({default: module[Icon]})
-            )
-        );
+            return lazy(() => 
+                import('react-icons/gi').then(module => 
+                    ({default: module[Icon]})
+                )
+            );
         }
         else if (iconGroup === 'Fi') {
-        return lazy(() => 
-            import('react-icons/fi').then(module => 
-            ({default: module[Icon]})
-            )
-        );
+            return lazy(() => 
+                import('react-icons/fi').then(module => 
+                    ({default: module[Icon]})
+                )
+            );
         }
         else if (iconGroup === 'Fa') {
-        return lazy(() => 
-            import('react-icons/fa').then(module => 
-            ({default: module[Icon]})
-            )
-        );
+            return lazy(() => 
+                import('react-icons/fa').then(module => 
+                    ({default: module[Icon]})
+                )
+            );
         }
         else if (iconGroup === 'Di') {
-        return lazy(() => 
-            import('react-icons/di').then(module => 
-            ({default: module[Icon]})
-            )
-        );
+            return lazy(() => 
+                import('react-icons/di').then(module => 
+                    ({default: module[Icon]})
+                )
+            );
         }
         else if (iconGroup === 'Ri') {
-        return lazy(() => 
-            import('react-icons/ri').then(module =>
-            ({default: module[Icon]})   
-            )
-        );
+            return lazy(() => 
+                import('react-icons/ri').then(module =>
+                    ({default: module[Icon]})   
+                )
+            );
+        }
+        else if (iconGroup === 'Gr') {
+            return lazy(() => 
+                import('react-icons/gr').then(module =>
+                    ({default: module[Icon]})   
+                )
+            );
+        }
+        else {
+            return ( <p>Not found.</p> );
         }
     }
 
@@ -58,25 +70,25 @@ export default function Skills() {
 						<p>Discover my current stack of tools, tech, and languages.</p>
 					</Col>
 				</Row>
-                <Row> {/* 
+                <Row> 
                     <Col xs={12} lg={4} className="animate-box">
-                        <SubsectionTitle className={"text-center"}>Languages</SubsectionTitle>
+                        <SubsectionTitle className={"text-center mb-2"}>Languages</SubsectionTitle>
                         <Row xs={3} className={'justify-content-center'}>
-                            <Skill skill="Python" icon='FaPython' />
-                            <Skill skill="C/C++" icon='FaCuttlefish' />
-                            <Skill skill="Javascript" Icon='DiJavascript1'/>
+                            <Skill skill="Python" Icon={loadIcon('FaPython')} />
+                            <Skill skill="C/C++" Icon={loadIcon('FaCuttlefish')} />
+                            <Skill skill="Javascript" Icon={loadIcon('DiJavascript1')} />
                         </Row>
                         <Row xs={3} className={'justify-content-center'}>
-                            <Skill skill="Java" Icon='DiJava'/>
-                            <Skill skill="Ruby" Icon='DiRuby' />
-                            <Skill skill="Bash" Icon='DiTerminal' />
+                            <Skill skill="Java" Icon={loadIcon('DiJava')} />
+                            <Skill skill="Ruby" Icon={loadIcon('DiRuby')} />
+                            <Skill skill="Bash" Icon={loadIcon('DiTerminal')} /> 
                         </Row>
                         <Row xs={3} className={'justify-content-center'}>
-                            <Skill skill="SQL" Icon='FaDatabase' />
+                            <Skill skill="SQL" Icon={loadIcon('FaDatabase')} />
                         </Row>
-                </Col> */}
+                    </Col> 
                     <Col xs={12} lg={4} id="middleSection">
-                        <SubsectionTitle className={"text-center"}>Technology</SubsectionTitle>
+                        <SubsectionTitle className={"text-center mb-2"}>Technology</SubsectionTitle>
                         <Row xs={3} className={'justify-content-center'}>
                             <Skill skill="MacOS" Icon={loadIcon('DiApple')} />
                             <Skill skill="GNU/Linux" Icon={loadIcon('DiGnu')} />
@@ -88,25 +100,24 @@ export default function Skills() {
                             <Skill skill="Node.js" Icon={loadIcon('DiNodejsSmall')} />
                         </Row>
                         <Row xs={3} className={'justify-content-center'}>
-                            <Skill skill="Flutter" Icon={loadIcon('RiFlutterLine')} /> 
                             <Skill skill="Ethereum" Icon={loadIcon('FaEthereum')} />
                         </Row>
-                    </Col> {/* 
+                    </Col>
                     <Col xs={12} lg={4}>
                         <SubsectionTitle className={'text-center'}>Tools</SubsectionTitle>
                         <Row xs={3} className={'justify-content-center'}>
-                            <Skill skill="Git" Icon='DiGit' />
-                            <Skill skill="GitHub" Icon='DiGithubBadge' />
-                            <Skill skill="Jenkins" Icon='DiJenkins' />
+                            <Skill skill="Git" Icon={loadIcon('DiGit')} />
+                            <Skill skill="GitHub" Icon={loadIcon('DiGithubBadge')} />
+                            <Skill skill="Jenkins" Icon={loadIcon('DiJenkins')} />
                         </Row>
                         <Row xs={3} className={'justify-content-center'}>
-                            <Skill skill="Gatsby" Icon="RiGatsbyLine" />
-                            <Skill skill="npm" Icon='DiNpm' />
-                            <Skill skill="Vim" Icon='DiVim' />
+                            <Skill skill="Gatsby" Icon={loadIcon('GrGatsbyjs')} />
+                            <Skill skill="npm" Icon={loadIcon('DiNpm')} />
+                            <Skill skill="Vim" Icon={loadIcon('DiVim')} />
                         </Row>
                         <Row xs={3} className={'justify-content-center'}>
-                            <Skill skill="Jira" Icon='FaJira' />
-                            <Skill skill="Slack" Icon='FaSlack' />
+                            <Skill skill="Jira" Icon={loadIcon('FaJira')} />
+                            <Skill skill="Slack" Icon={loadIcon('FaSlack')} />
                         </Row>
                     </Col>
                     */}
