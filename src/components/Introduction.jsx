220a653274640220227db8ef36be5873a258f339
Casey Key <casey.key@protonmail.com>
2020-08-03 13:53:08 -0600

    added button for resume

--- src/components/Introduction.jsx
+++ src/components/Introduction.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react'
-import { Container, Row, Col } from 'reactstrap'
+import { Container, Row, Col, Button } from 'reactstrap'
 import styled from 'styled-components'
 import { HeroWrap, Overlay, Text, Subheader, Header, Slider } from './Introduction/style.js'
 import FallingArrow from './Introduction/Mouse'
@@ -7,6 +7,7 @@ import Socials from './Social'
 import { Animated } from 'react-animated-css'
 import { graphql, useStaticQuery } from 'gatsby'
 import Typewriter from 'typewriter-effect/dist/core'
+import Resume from './Introduction/resume.pdf';
 
 export default function Introduction(props) {
   const data = useStaticQuery(
@@ -110,7 +111,14 @@ export default function Introduction(props) {
                       </Animated>
                     }
                   </Slider>
+
+                    <Button color="primary" href={Resume}>Resume</Button>
                 </Text>
+
+            </Col>
+          </Row>
+          <Row>
+            <Col>
             </Col>
           </Row>
         </Container>
