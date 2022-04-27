c757c745794781881b5ec1be19bf2af348e5dff5
Casey Key <casey.key@protonmail.com>
2022-04-27 10:12:01 -0700

    added import for dump

--- src/templates/blogPostTemplate.js
+++ src/templates/blogPostTemplate.js
@@ -2,7 +2,7 @@ import { graphql, Link } from 'gatsby'
 import { MDXRenderer } from 'gatsby-plugin-mdx'
 import React from 'react'
 import Layout from '../components/Layout'
-// import Dump from '../components/Dump'
+import Dump from '../components/Dump'
 
 export default function blogPostTemplate({ data, pageContext }) {
     
